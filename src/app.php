9ff4950bf82942895134c0ae103a8a7926b23b06
ulrik nielsen <ulrik@o3.dk>
2015-09-07 22:04:46 +0200

    remove ga tracking

--- src/app.php
+++ src/app.php
@@ -1,12 +1,22 @@
 <?php
 
 use Silex\Application;
+use Silex\Provider\HttpFragmentServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
 use Silex\Provider\ValidatorServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 
+// 3. party
+use Neutron\Silex\Provider\MongoDBODMServiceProvider;
+
 $app = new Application();
+
+// dirs.
+$app['root_dir']  = realpath(__DIR__.'/..');
+$app['cache_dir'] = $app['root_dir'].'/var/cache';
+$app['log_dir']   = $app['root_dir'].'/var/logs';
+
 $app->register(new UrlGeneratorServiceProvider());
 $app->register(new ValidatorServiceProvider());
 $app->register(new ServiceControllerServiceProvider());
@@ -17,6 +27,38 @@ $app['twig'] = $app->share($app->extend('twig', function ($twig, $app) {
     return $twig;
 }));
 
-$app->register(new Silex\Provider\HttpFragmentServiceProvider());
+
+$app->register(new HttpFragmentServiceProvider());
+
+
+// MongoDB
+$app->register(new MongoDBODMServiceProvider(), [
+    'doctrine.odm.mongodb.connection_options' => [
+        'database' => 'risteri_index',
+        'host'     => 'mongodb://localhost:27017',
+        'options'  => ['fsync' => false]
+    ],
+    'doctrine.odm.mongodb.documents' => [
+        0 => [
+            'type'      => 'annotation',
+            'path'      => [
+                $app['root_dir'].'/src/Model',
+            ],
+            'namespace' => 'Model',
+            'alias'     => 'Model',
+        ]
+    ],
+    'doctrine.odm.mongodb.proxies_dir'             => $app['cache_dir'].'/doctrine/odm/mongodb/Proxy',
+    'doctrine.odm.mongodb.proxies_namespace'       => 'DoctrineMongoDBProxy',
+    'doctrine.odm.mongodb.auto_generate_proxies'   => true,
+    'doctrine.odm.mongodb.hydrators_dir'           => $app['cache_dir'].'/doctrine/odm/mongodb/Hydrator',
+    'doctrine.odm.mongodb.hydrators_namespace'     => 'DoctrineMongoDBHydrator',
+    'doctrine.odm.mongodb.auto_generate_hydrators' => true,
+    'doctrine.odm.mongodb.metadata_cache'          => new \Doctrine\Common\Cache\ArrayCache(),
+    'doctrine.odm.mongodb.logger_callable'         => $app->protect(function($query) use ($app) {
+        $app['monolog']->debug('MongoDB query.', $query);
+    }),
+]);
+
 
 return $app;
